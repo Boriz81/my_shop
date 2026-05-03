90d65d17e1fbbbfc638abf0e622b0d427c053d46
Boris <blitzboriz@email.com>
2026-05-03 23:04:05 +0900

    cart 03.05.26

--- js/script.js
+++ js/script.js
@@ -9,20 +9,22 @@ const clearCartBtn = document.querySelector('#clear-cart-btn');
 const cart = [];
 
 function renderCart() {
-    cartCount.textContent = cart.length;
-    headerCartCount.textContent = cart.length;
+    let totalItems = 0;
+    cart.forEach(item => totalItems += item.quantity);
+    cartCount.textContent = totalItems;
+    headerCartCount.textContent = totalItems;
     
     let total = 0;
+    
     cartItems.innerHTML = '';
 
     cart.forEach(function(item) {
-        total += item.price;
-
+        total += item.price * item.quantity;
+        
         const li = document.createElement('li');
-        li.textContent = item.name + ' - ' + item.price + ' руб.';
+        li.textContent = item.name + 'x' + item.quantity + ' - ' + item.price + ' руб.';
         cartItems.appendChild(li);
     });
-
     cartTotal.textContent = total;
 }
 
@@ -30,14 +32,12 @@ buttons.forEach(function(button) {
     button.addEventListener('click', function() {
         const name = button.dataset.name;
         const price = Number(button.dataset.price);
-
-        cart.push({
-            name: name, 
-            price: price 
-        });
-
-        cartCount.textContent = cart.length;
-        headerCartCount.textContent = cart.length;
+        const existing = cart.find(item => item.name === name);
+        if (existing) {
+            existing.quantity += 1;
+        } else {
+            cart.push({ name, price, quantity: 1});
+        }
 
         renderCart();
     });
